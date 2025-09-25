f806d48b80488f9325a3adf05f7599db1cdef360
Rishika890 <rishika0620@gmail.com>
2025-09-26 00:50:13 +0530

    Add initial Rust hello world project

---
+++ target/debug/Polygon-pol-indexer.d
@@ -0,0 +1,1 @@
+C:\Users\rishika\OneDrive\Documents\Polygon-pol-indexer\target\debug\Polygon-pol-indexer.exe: C:\Users\rishika\OneDrive\Documents\Polygon-pol-indexer\src\main.rs
---
+++ target/debug/deps/Polygon_pol_indexer.d
@@ -0,0 +1,5 @@
+C:\Users\rishika\OneDrive\Documents\Polygon-pol-indexer\target\debug\deps\Polygon_pol_indexer.d: src\main.rs
+
+C:\Users\rishika\OneDrive\Documents\Polygon-pol-indexer\target\debug\deps\Polygon_pol_indexer.exe: src\main.rs
+
+src\main.rs:
